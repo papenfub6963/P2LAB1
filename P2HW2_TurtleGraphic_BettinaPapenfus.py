ab14d41da731ddf7d311050fa83f8f32cab0bdaf
papenfub6963 <55771514+papenfub6963@users.noreply.github.com>
2019-09-24 21:14:04 -0400

    Add files via upload

---
+++ P2HW2_TurtleGraphic_BettinaPapenfus.py
@@ -0,0 +1,21 @@
+# Turtle Graphic
+# September 24, 2019
+# CTI-110 P2HW2 - Turtle Graphic
+# Bettina Papenfus
+#
+
+import turtle
+pokey = turtle.Turtle()
+pokey.color('green')
+pokey.pensize(5)
+pokey.shape('turtle')
+
+pokey.forward(200)
+pokey.left(90)
+pokey.forward(200)
+pokey.right(90)
+
+pokey.backward(200)
+pokey.right(90)
+pokey.forward(200)
+pokey.left(90)
